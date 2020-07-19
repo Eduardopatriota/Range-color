1663c4c8be9c7b85220c19ae9680339615ed617e
Eduardopatriota <39865404+Eduardopatriota@users.noreply.github.com>
2020-07-19 17:06:06 -0300

    incluido troca de cor  nas letras do input

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ function changeColor(){
     let green = document.getElementById('rangeGreen').value;
     let blue = document.getElementById('rangeBlue').value;
     let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    let colorTexto = 'rgb(' + blue + ',' + green +',' + "155" + ')';
     console.log(color)
+    console.log("cor texto "+colorTexto)
     document.body.style.backgroundColor = color;
     document.getElementById('colorOutput').innerHTML = ': ' + color;
-
+    document.getElementById('label').style.color = colorTexto;
+    document.getElementById('label1').style.color = colorTexto;
+    document.getElementById('label2').style.color = colorTexto;
 }
 
 document.getElementById('rangeRed').addEventListener('input', changeColor);
 document.getElementById('rangeGreen').addEventListener('input', changeColor);
 document.getElementById('rangeBlue').addEventListener('input', changeColor);
+
+
+document.getElementsByTagName
